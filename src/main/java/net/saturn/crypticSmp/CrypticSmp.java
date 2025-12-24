12ad96673b45fe9246715ec9077eb6c648d55a67
Pumpkin <wishsmp1002@gmail.com>
2025-12-24 21:43:34 +1100

    ergerherh

--- src/main/java/net/saturn/crypticSmp/CrypticSmp.java
+++ src/main/java/net/saturn/crypticSmp/CrypticSmp.java
@@ -2,15 +2,20 @@ package net.saturn.crypticSmp;
 
 import net.saturn.crypticSmp.commands.CryptCommand;
 import net.saturn.crypticSmp.listeners.CryptListener;
+import net.saturn.crypticSmp.crypt.CryptManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CrypticSmp extends JavaPlugin {
-    private net.saturn.crypticSmp.crypt.CryptManager cryptManager;
+
+    private static CrypticSmp instance;
+    private CryptManager cryptManager;
 
     @Override
     public void onEnable() {
+        instance = this;
+
         // Initialize crypt manager
-        cryptManager = new net.saturn.crypticSmp.crypt.CryptManager(this);
+        cryptManager = new CryptManager(this);
 
         // Register commands
         CryptCommand cryptCommand = new CryptCommand(cryptManager);
@@ -18,7 +23,8 @@ public final class CrypticSmp extends JavaPlugin {
         getCommand("crypt").setTabCompleter(cryptCommand);
 
         // Register listeners
-        getServer().getPluginManager().registerEvents(new net.saturn.crypticSmp.listeners.CryptListener(cryptManager), this);
+        getServer().getPluginManager()
+                .registerEvents(new CryptListener(cryptManager), this);
 
         getLogger().info("CrypticSmp has been enabled!");
         getLogger().info("8 Crypts loaded successfully!");
@@ -26,10 +32,15 @@ public final class CrypticSmp extends JavaPlugin {
 
     @Override
     public void onDisable() {
+        instance = null;
         getLogger().info("CrypticSmp has been disabled!");
     }
 
-    public net.saturn.crypticSmp.crypt.CryptManager getCryptManager() {
+    public static CrypticSmp getInstance() {
+        return instance;
+    }
+
+    public CryptManager getCryptManager() {
         return cryptManager;
     }
 }
--- src/main/java/net/saturn/crypticSmp/crypt/types/UmbraeCrypt.java
+++ src/main/java/net/saturn/crypticSmp/crypt/types/UmbraeCrypt.java
@@ -4,8 +4,7 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.wrappers.*;
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
+import net.saturn.crypticSmp.CrypticSmp;
 import net.saturn.crypticSmp.crypt.CryptAbility;
 import net.saturn.crypticSmp.crypt.CryptManager;
 import org.bukkit.Location;
@@ -22,36 +21,42 @@ public class UmbraeCrypt implements CryptAbility {
     @Override
     public void applyPassive(Player player) {
         if (player.isSneaking() && player.getLocation().getBlock().getLightLevel() < 7) {
-            player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 40, 0, true, false));
+            player.addPotionEffect(
+                    new PotionEffect(PotionEffectType.INVISIBILITY, 40, 0, true, false)
+            );
         }
     }
 
     @Override
     public void useAbility1(Player player, CryptManager manager) {
+
         if (manager.isOnCooldown(player, "umbrae_ability1")) {
-            player.sendMessage("§cShadow Clone is on cooldown for " + manager.getRemainingCooldown(player, "umbrae_ability1") + "s");
+            player.sendMessage("§cShadow Clone is on cooldown for "
+                    + manager.getRemainingCooldown(player, "umbrae_ability1") + "s");
             return;
         }
 
         Location cloneLoc = player.getLocation().clone();
-        int entityId = new Random().nextInt(10000) + 100000;
+        int entityId = new Random().nextInt(100_000) + 1_000;
         UUID fakeUUID = UUID.randomUUID();
 
         try {
-            // Create GameProfile with player's skin
-            GameProfile profile = new GameProfile(fakeUUID, player.getName());
+            WrappedGameProfile wrappedProfile =
+                    new WrappedGameProfile(fakeUUID, player.getName());
 
-            // Copy skin properties
-            for (Property property : player.getPlayerProfile().getProperties()) {
-                profile.getProperties().put(property.getName(), property);
-            }
+            WrappedGameProfile realProfile =
+                    WrappedGameProfile.fromPlayer(player);
 
-            // Create wrapped game profile
-            WrappedGameProfile wrappedProfile = WrappedGameProfile.fromHandle(profile);
+            wrappedProfile.getProperties().putAll(realProfile.getProperties());
 
-            // Send player info packet to add the fake player to tab list
-            PacketContainer infoPacket = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.PLAYER_INFO);
-            infoPacket.getPlayerInfoActions().write(0, EnumSet.of(EnumWrappers.PlayerInfoAction.ADD_PLAYER));
+            PacketContainer infoPacket =
+                    ProtocolLibrary.getProtocolManager()
+                            .createPacket(PacketType.Play.Server.PLAYER_INFO);
+
+            infoPacket.getPlayerInfoActions().write(
+                    0,
+                    EnumSet.of(EnumWrappers.PlayerInfoAction.ADD_PLAYER)
+            );
 
             List<PlayerInfoData> data = new ArrayList<>();
             data.add(new PlayerInfoData(
@@ -60,10 +65,13 @@ public class UmbraeCrypt implements CryptAbility {
                     EnumWrappers.NativeGameMode.SURVIVAL,
                     WrappedChatComponent.fromText(player.getName())
             ));
+
             infoPacket.getPlayerInfoDataLists().write(1, data);
 
-            // Spawn the fake player
-            PacketContainer spawnPacket = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.NAMED_ENTITY_SPAWN);
+            PacketContainer spawnPacket =
+                    ProtocolLibrary.getProtocolManager()
+                            .createPacket(PacketType.Play.Server.NAMED_ENTITY_SPAWN);
+
             spawnPacket.getIntegers().write(0, entityId);
             spawnPacket.getUUIDs().write(0, fakeUUID);
             spawnPacket.getDoubles()
@@ -71,58 +79,60 @@ public class UmbraeCrypt implements CryptAbility {
                     .write(1, cloneLoc.getY())
                     .write(2, cloneLoc.getZ());
             spawnPacket.getBytes()
-                    .write(0, (byte) (cloneLoc.getYaw() * 256.0F / 360.0F))
-                    .write(1, (byte) (cloneLoc.getPitch() * 256.0F / 360.0F));
+                    .write(0, (byte) (cloneLoc.getYaw() * 256 / 360))
+                    .write(1, (byte) (cloneLoc.getPitch() * 256 / 360));
 
-            // Send packets to all nearby players
             for (Player other : player.getWorld().getPlayers()) {
-                if (!other.equals(player)) {
-                    ProtocolLibrary.getProtocolManager().sendServerPacket(other, infoPacket);
-                    ProtocolLibrary.getProtocolManager().sendServerPacket(other, spawnPacket);
+                if (other.equals(player)) continue;
 
-                    // Hide the real player
-                    other.hidePlayer(player.getServer().getPluginManager().getPlugin("CrypticSmp"), player);
-                }
+                ProtocolLibrary.getProtocolManager()
+                        .sendServerPacket(other, infoPacket);
+                ProtocolLibrary.getProtocolManager()
+                        .sendServerPacket(other, spawnPacket);
+
+                other.hidePlayer(CrypticSmp.getInstance(), player);
             }
 
             player.sendMessage("§8Shadow Clone active! You have 5 seconds to move.");
 
-            // After 5 seconds, remove the fake player and reveal the real one
             new BukkitRunnable() {
                 @Override
                 public void run() {
+
                     if (!player.isOnline()) return;
 
-                    try {
-                        // Remove entity packet
-                        PacketContainer destroyPacket = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.ENTITY_DESTROY);
-                        destroyPacket.getIntLists().write(0, Collections.singletonList(entityId));
+                    PacketContainer destroyPacket =
+                            ProtocolLibrary.getProtocolManager()
+                                    .createPacket(PacketType.Play.Server.ENTITY_DESTROY);
 
-                        // Remove from tab list
-                        PacketContainer removeInfoPacket = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.PLAYER_INFO_REMOVE);
-                        removeInfoPacket.getUUIDLists().write(0, Collections.singletonList(fakeUUID));
+                    destroyPacket.getIntLists()
+                            .write(0, Collections.singletonList(entityId));
 
-                        // Send to all nearby players
-                        for (Player other : player.getWorld().getPlayers()) {
-                            if (!other.equals(player)) {
-                                ProtocolLibrary.getProtocolManager().sendServerPacket(other, destroyPacket);
-                                ProtocolLibrary.getProtocolManager().sendServerPacket(other, removeInfoPacket);
+                    PacketContainer removeInfoPacket =
+                            ProtocolLibrary.getProtocolManager()
+                                    .createPacket(PacketType.Play.Server.PLAYER_INFO_REMOVE);
 
-                                // Show the real player at their new location
-                                other.showPlayer(player.getServer().getPluginManager().getPlugin("CrypticSmp"), player);
-                            }
-                        }
+                    removeInfoPacket.getUUIDLists()
+                            .write(0, Collections.singletonList(fakeUUID));
 
-                        player.sendMessage("§8Shadow Clone ended! You remain at your current location.");
-                    } catch (Exception e) {
-                        e.printStackTrace();
+                    for (Player other : player.getWorld().getPlayers()) {
+                        if (other.equals(player)) continue;
+
+                        ProtocolLibrary.getProtocolManager()
+                                .sendServerPacket(other, destroyPacket);
+                        ProtocolLibrary.getProtocolManager()
+                                .sendServerPacket(other, removeInfoPacket);
+
+                        other.showPlayer(CrypticSmp.getInstance(), player);
                     }
+
+                    player.sendMessage("§8Shadow Clone ended!");
                 }
-            }.runTaskLater(player.getServer().getPluginManager().getPlugin("CrypticSmp"), 100L);
+            }.runTaskLater(CrypticSmp.getInstance(), 100L);
 
         } catch (Exception e) {
-            player.sendMessage("§cFailed to create shadow clone!");
             e.printStackTrace();
+            player.sendMessage("§cFailed to create shadow clone!");
             return;
         }
 
@@ -131,28 +141,28 @@ public class UmbraeCrypt implements CryptAbility {
 
     @Override
     public void useAbility2(Player player, CryptManager manager) {
+
         if (manager.isOnCooldown(player, "umbrae_ability2")) {
-            player.sendMessage("§cFade Step is on cooldown for " + manager.getRemainingCooldown(player, "umbrae_ability2") + "s");
+            player.sendMessage("§cFade Step is on cooldown for "
+                    + manager.getRemainingCooldown(player, "umbrae_ability2") + "s");
             return;
         }
 
-        Vector direction = player.getLocation().getDirection().normalize().multiply(15);
+        Vector direction = player.getLocation().getDirection()
+                .normalize()
+                .multiply(15);
+
         Location newLoc = player.getLocation().add(direction);
+        newLoc.setYaw(player.getLocation().getYaw());
+        newLoc.setPitch(player.getLocation().getPitch());
 
-        // Keep the original yaw and pitch
-        float yaw = player.getLocation().getYaw();
-        float pitch = player.getLocation().getPitch();
-        newLoc.setYaw(yaw);
-        newLoc.setPitch(pitch);
-
-        // Ensure player doesn't teleport into walls
-        if (newLoc.getBlock().isPassable()) {
-            player.teleport(newLoc);
-            player.sendMessage("§8Fade Step!");
-        } else {
+        if (!newLoc.getBlock().isPassable()) {
             player.sendMessage("§cCannot teleport into solid blocks!");
             return;
         }
+
+        player.teleport(newLoc);
+        player.sendMessage("§8Fade Step!");
 
         manager.setCooldown(player, "umbrae_ability2", 30);
     }
